b1cd0ab3ffa5cff4cf66937576d0da9f87c8b0f1
Kyle Johannsen <kjohannsen1122@gmail.com>
2019-08-13 20:49:53 -0400

    Initial Commit

---
+++ RNDM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RNDM
+//
+//  Created by Kyle Johannsen on 8/13/19.
+//  Copyright © 2019 Kyle Johannsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
